a339f43a43e236067400393447d326d2c3698467
hangyeol0531 <jhan0531@naver.com>
2024-11-15 01:44:31 +0900

    react layout basic 3 grammar

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+const post = 'blog test title';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/* 
+        class로 하면 예약어 겹처서 jsx에서는 className 사용
+      */}
+      <div className="black-nav">
+        {/* 
+        style grammar style = {{}}
+        */}
+        <h4 style={{ color: 'red', fontSize: '16px' }}>blog</h4>
+      </div>
+      {/*
+       jsx 변수 중괄호 = (data binding)
+       */}
+      <h4>{post}</h4>
+    </div >
   );
 }
 
